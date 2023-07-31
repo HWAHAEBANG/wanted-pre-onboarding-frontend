2a93d6cfe7ce9e9c5d4709d9bbbd062a828091a2
화해방 <bcl0206@naver.com>
2023-07-31 13:52:03 +0900

    Chore: React-Icon 설치

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import { BiTask } from "react-icons/bi";
+import { MdOutlineKeyboardArrowRight } from "react-icons/md";
 
 export default function Header() {
   return (
     <div className='flex justify-between items-center w-full h-14 left-0'>
-      <Link to='/' className='text-blue-600 font-bold  text-2xl'>
+      <Link
+        to='/'
+        className='text-blue-600 font-bold  text-2xl flex items-center  gap-1'
+      >
+        <BiTask />
         Task Manager
       </Link>
       <div className='flex gap-10'>
-        <NavLink to='/' className='text-slate-500 relative'>
+        <NavLink to='/' className='text-slate-500 relative flex items-center'>
+          <MdOutlineKeyboardArrowRight />
           Home
         </NavLink>
-        <NavLink to='/todo' className='text-slate-500 relative'>
+        <NavLink
+          to='/todo'
+          className='text-slate-500 relative  flex items-center'
+        >
+          <MdOutlineKeyboardArrowRight />
           Todo List
         </NavLink>
       </div>
