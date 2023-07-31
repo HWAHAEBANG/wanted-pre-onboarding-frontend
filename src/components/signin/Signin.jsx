95e54f650b88177964490f97b976bb6ddfcad5bc
화해방 <bcl0206@naver.com>
2023-07-31 18:25:11 +0900

    Style: 로그인, 회원가입 UI 작업

--- src/components/signin/Signin.jsx
+++ src/components/signin/Signin.jsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import BlueButton from "../ui/BlueButton";
 
 export default function Signin() {
-  return <div>로그인</div>;
+  return (
+    <div className='h-screen flex flex-col justify-center items-center gap-5'>
+      <input placeholder='이메일' className={INPUT_STYLE} />
+      <input placeholder='비밀번호' className={INPUT_STYLE} />
+      <button data-testid='signup-button'>
+        <BlueButton text='로그인' />
+      </button>
+    </div>
+  );
 }
+
+const INPUT_STYLE = "w-96 h-10 rounded-md bg-white shadow-md outline-none px-3";
--- src/components/signup/Signup.jsx
+++ src/components/signup/Signup.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import BlueButton from "../ui/BlueButton";
+import { Link } from "react-router-dom";
 
 export default function Signup() {
-  return <div>회원가입</div>;
+  return (
+    <div className='h-screen flex flex-col justify-center items-center gap-5'>
+      <input placeholder='이메일' className={INPUT_STYLE} />
+      <input placeholder='비밀번호' className={INPUT_STYLE} />
+      <button data-testid='signup-button'>
+        <BlueButton text='회원가입' />
+      </button>
+      <p>
+        이미 계정이 있으신가요?&nbsp;
+        <Link to='/signin' className='decoration-solid'>
+          로그인
+        </Link>
+      </p>
+    </div>
+  );
 }
+
+const INPUT_STYLE = "w-96 h-10 rounded-md bg-white shadow-md outline-none px-3";
